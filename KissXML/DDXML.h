18ad54de93d5b5d412be40588d9d4336347d8bfd
deusty <none@none>
2009-05-26 21:14:23 +0000

    Adding element types to DDXML header
    --HG--
    extra : convert_revision : svn%3A69bd8580-3047-0410-960a-a116b6c96ec9/trunk%4095

--- KissXML/DDXML.h
+++ KissXML/DDXML.h
@@ -5,13 +5,53 @@
 #import "DDXMLDocument.h"
 
 #ifndef NSXMLNode
-#define NSXMLNode DDXMLNode
+  #define NSXMLNode DDXMLNode
 #endif
 #ifndef NSXMLElement
-#define NSXMLElement DDXMLElement
+  #define NSXMLElement DDXMLElement
 #endif
 #ifndef NSXMLDocument
-#define NSXMLDocument DDXMLDocument
+  #define NSXMLDocument DDXMLDocument
+#endif
+
+#ifndef NSXMLInvalidKind
+  #define NSXMLInvalidKind DDXMLInvalidKind
+#endif
+#ifndef NSXMLDocumentKind
+  #define NSXMLDocumentKind DDXMLDocumentKind
+#endif
+#ifndef NSXMLElementKind
+  #define NSXMLElementKind DDXMLElementKind
+#endif
+#ifndef NSXMLAttributeKind
+  #define NSXMLAttributeKind DDXMLAttributeKind
+#endif
+#ifndef NSXMLNamespaceKind
+  #define NSXMLNamespaceKind DDXMLNamespaceKind
+#endif
+#ifndef NSXMLProcessingInstructionKind
+  #define NSXMLProcessingInstructionKind DDXMLProcessingInstructionKind
+#endif
+#ifndef NSXMLCommentKind
+  #define NSXMLCommentKind DDXMLCommentKind
+#endif
+#ifndef NSXMLTextKind
+  #define NSXMLTextKind DDXMLTextKind
+#endif
+#ifndef NSXMLDTDKind
+  #define NSXMLDTDKind DDXMLDTDKind
+#endif
+#ifndef NSXMLEntityDeclarationKind
+  #define NSXMLEntityDeclarationKind DDXMLEntityDeclarationKind
+#endif
+#ifndef NSXMLAttributeDeclarationKind
+  #define NSXMLAttributeDeclarationKind DDXMLAttributeDeclarationKind
+#endif
+#ifndef NSXMLElementDeclarationKind
+  #define NSXMLElementDeclarationKind DDXMLElementDeclarationKind
+#endif
+#ifndef NSXMLNotationDeclarationKind
+  #define NSXMLNotationDeclarationKind DDXMLNotationDeclarationKind
 #endif
 
 #endif
