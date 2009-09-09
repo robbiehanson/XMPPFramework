d12bc9a4a6267df2969825f7a95ba6f9d2e8da1b
deusty <none@none>
2009-09-09 04:04:56 +0000

    Improved performance on iPhone, take 2.
    --HG--
    extra : convert_revision : svn%3A69bd8580-3047-0410-960a-a116b6c96ec9/trunk%40157

--- XMPPParser.m
+++ XMPPParser.m
@@ -1,42 +1,19 @@
 #import "XMPPParser.h"
 #import "DDXMLPrivate.h"
 
-/**
- * How does this class work?
- * 
- * The libxml DOM interface is written on top of their SAX interface.
- * So to build a DOM tree from a document, libxml creates a SAX parser,
- * and then proceeds to iteratively create the tree from within the SAX callback functions.
- * 
- * What we do is monitor the SAX callbacks (AKA the DOM tree building callbacks)
- * to keep tabs on when elements are coming in.
- * When we see a complete element, then we simply extract it from the tree, and pass it to the delegate.
-**/
+#define CHECK_FOR_NULL(value) do { if(value == NULL) { xmlAbortDueToMemoryShortage(ctxt); return; } } while(false)
+
 
 @implementation XMPPParser
 
 /**
- * Our override for the SAX start element callback.
- * We use this method to keep tabs on where we are within the document.
- * Ultimately, we want to know when we've finished reading a full xmpp node so we can pass it to the delegate.
+ * This method is called at the end of the xmlStartElement method.
+ * This allows us to inspect the parser and xml tree, and determine if we need to invoke any delegate methods.
 **/
-static void	onStartElementNs(void *ctx, const xmlChar  *localname,
-                                        const xmlChar  *prefix,
-                                        const xmlChar  *URI,
-                                                  int   nb_namespaces,
-                                        const xmlChar **namespaces,
-                                                  int   nb_attributes,
-                                                  int   nb_defaulted,
-                                        const xmlChar **attributes)
+static void postStartElement(xmlParserCtxt *ctxt)
 {
-	xmlParserCtxt *ctxt = (xmlParserCtxt *)ctx;
-	xmlSAXHandler *hdlr = ctxt->sax;
-	
-	XMPPParser *parser = (XMPPParser *)hdlr->_private;
-	
+	XMPPParser *parser = (XMPPParser *)ctxt->_private;
 	parser->depth++;
-	parser->domStartElementNs(ctxt, localname, prefix, URI, nb_namespaces, namespaces,
-	                          nb_attributes, nb_defaulted, attributes);
 	
 	if(!(parser->hasReportedRoot) && (parser->depth == 1))
 	{
@@ -75,16 +52,13 @@ static void	onStartElementNs(void *ctx, const xmlChar  *localname,
 	}
 }
 
-static void onEndElementNs(void *ctx, const xmlChar *localname,
-                                      const xmlChar *prefix,
-                                      const xmlChar *URI)
+/**
+ * This method is called at the end of the xmlEndElement method.
+ * This allows us to inspect the parser and xml tree, and determine if we need to invoke any delegate methods.
+**/
+static void postEndElement(xmlParserCtxt *ctxt)
 {
-	xmlParserCtxt *ctxt = (xmlParserCtxt *)ctx;
-	xmlSAXHandler *hdlr = ctxt->sax;
-	
-	XMPPParser *parser = (XMPPParser *)hdlr->_private;
-	
-	parser->domEndElementNs(ctx, localname, prefix, URI);
+	XMPPParser *parser = (XMPPParser *)ctxt->_private;
 	parser->depth--;
 	
 	if(parser->depth == 1)
@@ -101,8 +75,8 @@ static void onEndElementNs(void *ctx, const xmlChar *localname,
 		{
 			if(child->type == XML_ELEMENT_NODE)
 			{
-				[DDXMLNode detachChild:child fromNode:root];
 				DDXMLElement *childWrapper = [DDXMLElement nodeWithPrimitive:(xmlKindPtr)child];
+				[childWrapper detach];
 				
 				// Note: We want to detach the child from the root even if the delegate method isn't setup.
 				// This prevents the doc from growing infinitely large.
@@ -131,6 +105,205 @@ static void onEndElementNs(void *ctx, const xmlChar *localname,
 	}
 }
 
+/**
+ * We're screwed...
+**/
+static void xmlAbortDueToMemoryShortage(xmlParserCtxt *ctxt)
+{
+	XMPPParser *parser = (XMPPParser *)ctxt->_private;
+	
+	xmlStopParser(ctxt);
+	
+	if([parser->delegate respondsToSelector:@selector(xmppParser:didFail:)])
+	{
+		NSString *errMsg = @"Unable to allocate memory in xmpp parser";
+		NSDictionary *info = [NSDictionary dictionaryWithObject:errMsg forKey:NSLocalizedDescriptionKey];
+		
+		NSError *error = [NSError errorWithDomain:@"libxmlErrorDomain" code:1001 userInfo:info];
+		
+		[parser->delegate xmppParser:parser didFail:error];
+	}
+}
+
+/**
+ * SAX parser C-style callback.
+ * Invoked when a new node element is started.
+**/
+static void	xmlStartElement(void *ctx, const xmlChar  *nodeName,
+                                       const xmlChar  *nodePrefix,
+                                       const xmlChar  *nodeUri,
+                                                 int   nb_namespaces,
+                                       const xmlChar **namespaces,
+                                                 int   nb_attributes,
+                                                 int   nb_defaulted,
+                                       const xmlChar **attributes)
+{
+	int i, j;
+	xmlNsPtr lastAddedNs;
+	
+	xmlParserCtxt *ctxt = (xmlParserCtxt *)ctx;
+	
+	// We store the parent node in the context's node pointer.
+	// We keep this updated by "pushing" the node in the startElement method,
+	// and "popping" the node in the endElement method.
+	xmlNodePtr parent = ctxt->node;
+	
+	// Create the node
+	xmlNodePtr newNode = xmlNewDocNode(ctxt->myDoc, NULL, nodeName, NULL);
+	CHECK_FOR_NULL(newNode);
+	
+	// Add the node to the tree
+	if(parent == NULL)
+	{
+		// Root node
+		xmlAddChild((xmlNodePtr)ctxt->myDoc, newNode);
+	}
+	else
+	{
+		xmlAddChild(parent, newNode);
+	}
+	
+	// Process the namespaces
+	for(i = 0, j = 0; j < nb_namespaces; j++)
+	{
+		// Extract namespace prefix and uri
+		const xmlChar *nsPrefix = namespaces[i++];
+		const xmlChar *nsUri    = namespaces[i++];
+		
+		// Create the namespace
+		xmlNsPtr newNs = xmlNewNs(NULL, nsUri, nsPrefix);
+		CHECK_FOR_NULL(newNs);
+		
+		// Add namespace to node.
+		// Each node has a linked list of nodes (in the nsDef variable).
+		// The linked list is forward only.
+		// In other words, each ns has a next, but not a prev pointer.
+		
+		if (newNode->nsDef == NULL)
+		{
+			newNode->nsDef = lastAddedNs = newNs;
+		}
+		else
+		{
+			lastAddedNs->next = newNs;
+			lastAddedNs = newNs;
+		}
+		
+		if ((nodeUri != NULL) && (nodePrefix == nsPrefix))
+		{
+			// This is the namespace for the node.
+			// Example 1: the node was <stream:stream xmlns:stream="url"> and newNs is the stream:url namespace.
+			// Example 2: the node was <starttls xmlns="url"> and newNs is the null:url namespace.
+			newNode->ns = newNs;
+		}
+	}
+	
+	// Search for the node's namespace if it wasn't already found
+	if ((nodeUri != NULL) && (newNode->ns == NULL))
+	{
+		// xmlSearchNs(xmlDocPtr doc, xmlNodePtr node, const xmlChar *nameSpace)
+		// 
+		// Search a Ns registered under a given name space for a document.
+		// Recurse on the parents until it finds the defined namespace or return NULL otherwise.
+		// The nameSpace parameter can be NULL, this is a search for the default namespace.
+		
+		newNode->ns = xmlSearchNs(ctxt->myDoc, parent, nodePrefix);
+		
+		if (newNode->ns == NULL)
+		{
+			// We use href==NULL in the case of an element creation where the namespace was not defined.
+			newNode->ns = xmlNewNs(newNode, NULL, nodePrefix);
+			CHECK_FOR_NULL(newNode->ns);
+		}
+	}
+	
+	// Process all the attributes
+	for (i = 0, j = 0; j < nb_attributes; j++)
+	{
+		const xmlChar *attrName   = attributes[i++];
+		const xmlChar *attrPrefix = attributes[i++];
+		const xmlChar *attrUri    = attributes[i++];
+		const xmlChar *valueBegin = attributes[i++];
+		const xmlChar *valueEnd   = attributes[i++];
+		
+		xmlChar *value = xmlStrndup(valueBegin, valueEnd - valueBegin);
+		CHECK_FOR_NULL(value);
+		
+		if ((attrPrefix == NULL) && (attrUri == NULL))
+		{
+			// Normal attribute - no associated namespace
+			xmlAttrPtr newAttr = xmlNewProp(newNode, attrName, value);
+			CHECK_FOR_NULL(newAttr);
+		}
+		else
+		{
+			// Find the namespace for the attribute
+			xmlNsPtr attrNs = xmlSearchNs(ctxt->myDoc, newNode, attrPrefix);
+			
+			if(attrNs != NULL)
+			{
+				xmlAttrPtr newAttr = xmlNewNsProp(newNode, attrNs, attrName, value);
+				CHECK_FOR_NULL(newAttr);
+			}
+			else
+			{
+				attrNs = xmlNewNs(NULL, NULL, nodePrefix);
+				CHECK_FOR_NULL(attrNs);
+				
+				xmlAttrPtr newAttr = xmlNewNsProp(newNode, attrNs, attrName, value);
+				CHECK_FOR_NULL(newAttr);
+			}
+		}
+		
+		xmlFree(value);
+	}
+	
+	// Update our parent node pointer
+	ctxt->node = newNode;
+	
+	// Invoke delegate methods if needed
+	postStartElement(ctxt);
+}
+
+/**
+ * SAX parser C-style callback.
+ * Invoked when characters are found within a node.
+**/
+static void xmlCharacters(void *ctx, const xmlChar *ch, int len)
+{
+	xmlParserCtxt *ctxt = (xmlParserCtxt *)ctx;
+	
+	if(ctxt->node != NULL)
+	{
+		xmlNodePtr textNode = xmlNewTextLen(ch, len);
+		
+		// xmlAddChild(xmlNodePtr parent, xmlNodePtr cur)
+		// 
+		// Add a new node to @parent, at the end of the child list
+		// merging adjacent TEXT nodes (in which case @cur is freed).
+		
+		xmlAddChild(ctxt->node, textNode);
+	}
+}
+
+/**
+ * SAX parser C-style callback.
+ * Invoked when a new node element is ended.
+**/
+static void xmlEndElement(void *ctx, const xmlChar *localname,
+                                     const xmlChar *prefix,
+                                     const xmlChar *URI)
+{
+	xmlParserCtxt *ctxt = (xmlParserCtxt *)ctx;
+	
+	// Update our parent node pointer
+	if(ctxt->node != NULL)
+		ctxt->node = ctxt->node->parent;
+	
+	// Invoke delegate methods if needed
+	postEndElement(ctxt);
+}
+
 - (id)initWithDelegate:(id)aDelegate
 {
 	if((self = [super init]))
@@ -144,32 +317,25 @@ static void onEndElementNs(void *ctx, const xmlChar *localname,
 		xmlSAXHandler saxHandler;
 		memset(&saxHandler, 0, sizeof(xmlSAXHandler));
 		
-		// Initialize handler to default values,
-		// which will point each SAX callback function to the DOM tree-building functions.
-		xmlSAXVersion(&saxHandler, 2);
-		
-		// Store references to original functions (before we override them)
-		domStartElementNs = saxHandler.startElementNs;
-		domEndElementNs   = saxHandler.endElementNs;
-		
-		// Tell libxml not to keep ignorable whitespace (such as node indentation, formatting, etc).
-		// NSXML ignores such whitespace.
-		// This also has the added benefit of taking up less RAM when parsing formatted XML documents.
-		saxHandler.ignorableWhitespace = xmlSAX2IgnorableWhitespace;
-		
-		// Setup overrides, and store reference to ourself for use in the C-style callback functions
-		saxHandler.startElementNs = onStartElementNs;
-		saxHandler.endElementNs   = onEndElementNs;
-		saxHandler._private       = self;
+		saxHandler.initialized = XML_SAX2_MAGIC;
+		saxHandler.startElementNs = xmlStartElement;
+		saxHandler.characters = xmlCharacters;
+		saxHandler.endElementNs = xmlEndElement;
 		
 		// Create the push parser context
 		parserCtxt = xmlCreatePushParserCtxt(&saxHandler, NULL, NULL, 0, NULL);
 		
 		// Note: This method copies the saxHandler, so we don't have to keep it around.
 		
-		// Note: The parserCtxt also has a useData variable, but it is used by the DOM building functions.
+		// Create the document to hold the parsed elements
+		parserCtxt->myDoc = xmlNewDoc(parserCtxt->version);
+		
+		// Store reference to ourself
+		parserCtxt->_private = self;
+		
+		// Note: The parserCtxt also has a userData variable, but it is used by the DOM building functions.
 		// If we put a value there, it actually causes a crash!
-		// So instead we add a reference to ourself in the saxHandler.
+		// We need to be sure to use the _private variable which libxml won't touch.
 	}
 	return self;
 }
@@ -179,7 +345,6 @@ static void onEndElementNs(void *ctx, const xmlChar *localname,
 	if(parserCtxt)
 	{
 		// The xmlFreeParserCtxt method will not free the created document in parserCtxt->myDoc.
-		// It will also not free the application data in sax->_private either (obviously).
 		
 		if(parserCtxt->myDoc)
 		{
