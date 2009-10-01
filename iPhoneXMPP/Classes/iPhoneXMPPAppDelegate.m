647858afe7c623e2e67d0815734498de395e35a5
Robbie Hanson <robbiehanson@deusty.com>
2009-10-01 17:08:34 +0000

    Adding simple template to iphone project.

--- iPhoneXMPP/Classes/iPhoneXMPPAppDelegate.m
+++ iPhoneXMPP/Classes/iPhoneXMPPAppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "iPhoneXMPPAppDelegate.h"
 #import "RootViewController.h"
-
+#import "XMPP.h"
 
 @implementation iPhoneXMPPAppDelegate
 
@@ -16,7 +16,27 @@
 @synthesize navigationController;
 
 
-- (void)applicationDidFinishLaunching:(UIApplication *)application {
+- (void)applicationDidFinishLaunching:(UIApplication *)application
+{
+	xmppClient = [[XMPPClient alloc] init];
+	[xmppClient addDelegate:self];
+	
+	// Replace me with the proper domain and port.
+	// The example below is setup for a typical google talk account.
+//	[xmppClient setDomain:@"talk.google.com"];
+//	[xmppClient setPort:5222];
+	
+	// Replace me with the proper JID and password
+//	[xmppClient setMyJID:[XMPPJID jidWithString:@"username@gmail.com/iPhoneTest"]];
+//	[xmppClient setPassword:@"password"];
+	
+	[xmppClient setAutoLogin:YES];
+	[xmppClient setAllowsPlaintextAuth:NO];
+	[xmppClient setAutoPresence:YES];
+	[xmppClient setAutoRoster:YES];
+	
+	// Uncomment me when the proper information has been entered above.
+//	[xmppClient connect];
 	
 	// Configure and show the window
 	[window addSubview:[navigationController view]];
@@ -33,6 +53,94 @@
 	[navigationController release];
 	[window release];
 	[super dealloc];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark XMPPClient Delegate Methods:
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)xmppClientConnecting:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientConnecting");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClientDidConnect:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientDidConnect");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClientDidNotConnect:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientDidNotConnect");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClientDidDisconnect:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientDidDisconnect");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClientDidRegister:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientDidRegister");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClient:(XMPPClient *)sender didNotRegister:(NSXMLElement *)error
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClient:didNotRegister: %@", error);
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClientDidAuthenticate:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientDidAuthenticate");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClient:(XMPPClient *)sender didNotAuthenticate:(NSXMLElement *)error
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClient:didNotAuthenticate: %@", error);
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClientDidUpdateRoster:(XMPPClient *)sender
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClientDidUpdateRoster");
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClient:(XMPPClient *)sender didReceiveBuddyRequest:(XMPPJID *)jid
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClient:didReceiveBuddyRequest: %@", jid);
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClient:(XMPPClient *)sender didReceiveIQ:(XMPPIQ *)iq
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClient:didReceiveIQ: %@", iq);
+	NSLog(@"==============================================================");
+}
+
+- (void)xmppClient:(XMPPClient *)sender didReceiveMessage:(XMPPMessage *)message
+{
+	NSLog(@"==============================================================");
+	NSLog(@"iPhoneXMPPAppDelegate: xmppClient:didReceiveMessage: %@", message);
+	NSLog(@"==============================================================");
 }
 
 @end
