30e25a4272636b4de085c4c557b0043842283cd0
Robbie Hanson <robbiehanson@deusty.com>
2009-10-26 19:01:54 +0000

    Updated iPhoneXMPP project to work properly with simulator on snow leopard.

--- iPhoneXMPP/Classes/iPhoneXMPPAppDelegate.h
+++ iPhoneXMPP/Classes/iPhoneXMPPAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  iPhoneXMPPAppDelegate.h
-//  iPhoneXMPP
-//
-//  Created by Robbie Hanson on 5/26/09.
-//  Copyright Deusty Designs, LLC. 2009. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @class XMPPClient;
@@ -16,11 +8,9 @@
 	XMPPClient *xmppClient;
     
     UIWindow *window;
-    UINavigationController *navigationController;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
 
 @end
 
