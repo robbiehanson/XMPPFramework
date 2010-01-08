76debac3c892f701feb5c0e6ef4165fdc97cbbbf
Robbie Hanson <robbiehanson@deusty.com>
2010-01-08 15:53:59 +0000

    No more isa-swizzling. Now using the ObjC 2.0 method instead.

--- XMPPIQ.m
+++ XMPPIQ.m
@@ -1,14 +1,38 @@
 #import "XMPPIQ.h"
 #import "NSXMLElementAdditions.h"
 
+#import <objc/objc-runtime.h>
+
 
 @implementation XMPPIQ
 
++ (void)initialize
+{
+	// We use the object_setClass method below to dynamically change the class from a standard NSXMLElement.
+	// The size of the two classes is expected to be the same.
+	// 
+	// If a developer adds instance methods to this class, bad things happen at runtime that are very hard to debug.
+	// This check is here to aid future developers who may make this mistake.
+	// 
+	// For Fearless And Experienced Objective-C Developers:
+	// It may be possible to support adding instance variables to this class if you seriously need it.
+	// To do so, try realloc'ing self after altering the class, and then initialize your variables.
+	
+	size_t superSize = class_getInstanceSize([NSXMLElement class]);
+	size_t ourSize   = class_getInstanceSize([XMPPIQ class]);
+	
+	if (superSize != ourSize)
+	{
+		NSLog(@"Adding instance variables to XMPPIQ is not currently supported!");
+		exit(15);
+	}
+}
+
 + (XMPPIQ *)iqFromElement:(NSXMLElement *)element
 {
-	XMPPIQ *result = (XMPPIQ *)element;
-	result->isa = [XMPPIQ class];
-	return result;
+	object_setClass(element, [XMPPIQ class]);
+	
+	return (XMPPIQ *)element;
 }
 
 /**
