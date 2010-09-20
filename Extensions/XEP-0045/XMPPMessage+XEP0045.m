b3d29505f67822fef2eb83597160c7875207c613
Eric Chamberlain <eric.chamberlain@gmail.com>
2010-09-20 20:15:07 +0000

    Added XMPPMessage category for XEP-0045. Fixed leak in XMPPDateTimeProfiles.m

---
+++ Extensions/XEP-0045/XMPPMessage+XEP0045.m
@@ -0,0 +1,29 @@
+//
+//  XMPPMessage+XEP0045.m
+//  talk
+//
+//  Created by Eric Chamberlain on 9/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "XMPPMessage+XEP0045.h"
+#import "NSXMLElementAdditions.h"
+
+
+@implementation XMPPMessage(XEP0045)
+
+- (BOOL)isGroupChatMessage {
+    return [[[self attributeForName:@"type"] stringValue] isEqualToString:@"groupchat"];
+}
+
+- (BOOL)isGroupChatMessageWithBody {
+    if([self isGroupChatMessage]) {
+        NSString *body = [[self elementForName:@"body"] stringValue];
+        
+        return ((body != nil) && ([body length] > 0));
+    }
+    
+    return NO;
+}
+
+@end
