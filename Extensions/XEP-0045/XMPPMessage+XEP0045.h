bae199c3161e7f58744a63aa64779fd96c23a988
Eric Chamberlain <eric.chamberlain@gmail.com>
2010-09-20 20:20:06 +0000

    Fix file comments comments.

--- Extensions/XEP-0045/XMPPMessage+XEP0045.h
+++ Extensions/XEP-0045/XMPPMessage+XEP0045.h
@@ -1,9 +1,8 @@
 //
 //  XMPPMessage+XEP0045.h
-//  talk
 //
 //  Created by Eric Chamberlain on 9/20/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 RF.com. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
