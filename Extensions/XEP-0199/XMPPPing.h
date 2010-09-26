7d38e012e2125c8bb52cc3d570dec5bc80a58d24
deusty <none@none>
2010-09-26 23:59:42 +0000

    Improving XEP-0199 (ping) implementation.
    --HG--
    extra : convert_revision : svn%3A69bd8580-3047-0410-960a-a116b6c96ec9/trunk%40256

--- Extensions/XEP-0199/XMPPPing.h
+++ Extensions/XEP-0199/XMPPPing.h
@@ -9,13 +9,27 @@
 
 @interface XMPPPing : XMPPModule
 {
-	NSMutableArray *pingIDs;
+	NSMutableDictionary *pingIDs;
 }
 
 - (id)initWithStream:(XMPPStream *)xmppStream;
 
-- (void)sendPingToServer;
-- (void)sendPingToJID:(XMPPJID *)jid;
+/**
+ * Send pings to the server or a specific JID.
+ * The disco module may be used to detect if the target supports ping.
+ * 
+ * The returned string is the pingID (the elementID of the query that was sent).
+ * In other words:
+ * 
+ * SEND: <iq id="<returned_string>" type="get" .../>
+ * RECV: <iq id="<returned_string>" type="result" .../>
+ * 
+ * This may be helpful if you are sending multiple simultaneous pings to the same target.
+**/
+- (NSString *)sendPingToServer;
+- (NSString *)sendPingToServerWithTimeout:(NSTimeInterval)timeout;
+- (NSString *)sendPingToJID:(XMPPJID *)jid;
+- (NSString *)sendPingToJID:(XMPPJID *)jid withTimeout:(NSTimeInterval)timeout;
 
 @end
 
@@ -23,5 +37,6 @@
 @optional
 
 - (void)xmppPing:(XMPPPing *)sender didReceivePong:(XMPPIQ *)pong;
+- (void)xmppPing:(XMPPPing *)sender didNotReceivePong:(NSString *)pingID dueToTimeout:(NSTimeInterval)timeout;
 
 @end
