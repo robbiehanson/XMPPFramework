51dd76eca942597e2b515b45e3805c349fc8c1eb
eric.chamberlain <none@none>
2010-10-14 18:16:56 +0000

    XMPPStream:  * Fixed memory leak in XMPPStream, userTag was not released.  * Fixed compile warnings when used with Three20, added some casts to suppress compiler warnings.  * Moved some settings to .h for subclassing
    Added support for X-FACEBOOK-PLATFORM auth.  The new XMPPStreamFacebook class supports the latest version of the Facebook GraphAPI, see the mailing list for usage instructions.
    
    --HG--
    extra : convert_revision : svn%3A69bd8580-3047-0410-960a-a116b6c96ec9/trunk%40265

--- Core/XMPPStream.m
+++ Core/XMPPStream.m
@@ -17,45 +17,6 @@
 #endif
 
 
-// Define the debugging state
-#define DEBUG_SEND      YES
-#define DEBUG_RECV_PRE  YES  // Prints data before going to xmpp parser
-#define DEBUG_RECV_POST NO   // Prints data as it comes out of xmpp parser
-
-#define DDLogSend(format, ...)     do{ if(DEBUG_SEND)      NSLog((format), ##__VA_ARGS__); }while(0)
-#define DDLogRecvPre(format, ...)  do{ if(DEBUG_RECV_PRE)  NSLog((format), ##__VA_ARGS__); }while(0)
-#define DDLogRecvPost(format, ...) do{ if(DEBUG_RECV_POST) NSLog((format), ##__VA_ARGS__); }while(0)
-
-// Define the various timeouts (in seconds) for retreiving various parts of the XML stream
-#define TIMEOUT_WRITE         10
-#define TIMEOUT_READ_START    10
-#define TIMEOUT_READ_STREAM   -1
-
-// Define the various tags we'll use to differentiate what it is we're currently reading or writing
-#define TAG_WRITE_START        -100 // Must be outside UInt16 range
-#define TAG_WRITE_STREAM       -101 // Must be outside UInt16 range
-#define TAG_WRITE_SYNCHRONOUS  -102 // Must be outside UInt16 range
-
-#define TAG_READ_START       200
-#define TAG_READ_STREAM      201
-
-// Define the various states we'll use to track our progress
-enum {
-	STATE_DISCONNECTED,
-	STATE_RESOLVING_SRV,
-	STATE_CONNECTING,
-	STATE_OPENING,
-	STATE_NEGOTIATING,
-	STATE_STARTTLS,
-	STATE_REGISTERING,
-	STATE_AUTH_1,
-	STATE_AUTH_2,
-	STATE_AUTH_3,
-	STATE_BINDING,
-	STATE_START_SESSION,
-	STATE_CONNECTED,
-};
-
 #if TARGET_OS_IPHONE
   #define SOCKET_BUFFER_SIZE 512  // bytes
 #else
@@ -145,7 +106,7 @@ enum XMPPStreamFlags
 	numberOfBytesSent = 0;
 	numberOfBytesReceived = 0;
 	
-	parser = [[XMPPParser alloc] initWithDelegate:self];
+	parser = [(XMPPParser *)[XMPPParser alloc] initWithDelegate:self];
 	
 	hostPort = 5222;
 	keepAliveInterval = DEFAULT_KEEPALIVE_INTERVAL;
@@ -166,7 +127,7 @@ enum XMPPStreamFlags
 		[self commonInit];
 		
 		// Initialize socket
-		asyncSocket = [[AsyncSocket alloc] initWithDelegate:self];
+		asyncSocket = [(AsyncSocket *)[AsyncSocket alloc] initWithDelegate:self];
 		
 		// Initialize configuration
 		flags = 0;
@@ -231,6 +192,8 @@ enum XMPPStreamFlags
 	[srvResults release];
 	
 	[synchronousUUID release];
+    
+    [userTag release];
 	
 	[super dealloc];
 }
@@ -467,7 +430,7 @@ enum XMPPStreamFlags
 	state = STATE_CONNECTING;
 	
 	// Initailize socket
-	asyncSocket = [[AsyncSocket alloc] initWithDelegate:self];
+	asyncSocket = [(AsyncSocket *)[AsyncSocket alloc] initWithDelegate:self];
 	
 	BOOL result = [asyncSocket connectToAddress:remoteAddr error:errPtr];
 	
@@ -1326,12 +1289,12 @@ enum XMPPStreamFlags
 		[parser setDelegate:nil];
 		[parser release];
 		
-		parser = [[XMPPParser alloc] initWithDelegate:self];
+		parser = [(XMPPParser *)[XMPPParser alloc] initWithDelegate:self];
 	}
 	else if (parser == nil)
 	{
 		// Need to create parser (it was destroyed when the socket was last disconnected)
-		parser = [[XMPPParser alloc] initWithDelegate:self];
+		parser = [(XMPPParser *)[XMPPParser alloc] initWithDelegate:self];
 	}
 	
 	NSString *xmlns = @"jabber:client";
@@ -1763,7 +1726,7 @@ enum XMPPStreamFlags
 	}
 	else
 	{
-		// We are successfully authenticated (via sasl:plain)
+		// We are successfully authenticated (via sasl:x-facebook-platform or sasl:plain)
 		[self setIsAuthenticated:YES];
 		
 		// Now we start our negotiation over again...
@@ -2248,7 +2211,7 @@ enum XMPPStreamFlags
 	}
 	else if(state == STATE_AUTH_3)
 	{
-		// The response from our authenticateAnonymously challenge
+		// The response from our x-facebook-platform or authenticateAnonymously challenge
 		[self handleAuth3:element];
 	}
 	else if(state == STATE_BINDING)
