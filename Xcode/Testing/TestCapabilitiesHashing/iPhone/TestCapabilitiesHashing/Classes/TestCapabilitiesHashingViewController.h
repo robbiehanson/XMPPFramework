440ff8f2d462c0d019c4b18280d45b5a2dbe7828
Robbie Hanson <robbiehanson@voalte.com>
2011-02-09 17:23:42 -0500

    Adding UnitTest for XMPPCapabilities (XEP-0115)

---
+++ Xcode/Testing/TestCapabilitiesHashing/iPhone/TestCapabilitiesHashing/Classes/TestCapabilitiesHashingViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestCapabilitiesHashingViewController.h
+//  TestCapabilitiesHashing
+//
+//  Created by Robbie Hanson on 5/12/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestCapabilitiesHashingViewController : UIViewController {
+
+}
+
+@end
+
