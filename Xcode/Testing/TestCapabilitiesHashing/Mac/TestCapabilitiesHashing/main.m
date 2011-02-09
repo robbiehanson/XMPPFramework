a26687c9cd68790b435877bcd76dbb93d34e9487
Robbie Hanson <robbiehanson@voalte.com>
2011-02-09 17:23:42 -0500

    Adding UnitTest for XMPPCapabilities (XEP-0115)

---
+++ Xcode/Testing/TestCapabilitiesHashing/Mac/TestCapabilitiesHashing/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TestCapabilitiesHashing
+//
+//  Created by Robbie Hanson on 5/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
