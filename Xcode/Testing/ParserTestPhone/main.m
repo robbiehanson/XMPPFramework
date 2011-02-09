d6f5c7dac3a6418af310e10789c2c2d937c0178d
Robbie Hanson <robbiehanson@voalte.com>
2011-02-09 16:27:43 -0500

    Adding UnitTest for XMPPParser on iOS

---
+++ Xcode/Testing/ParserTestPhone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ParserTestPhone
+//
+//  Created by Robbie Hanson on 11/22/09.
+//  Copyright Deusty Designs, LLC. 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
