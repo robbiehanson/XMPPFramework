cfc2813c60a33422af8729649b6a24505f617943
eric.chamberlain@gmail.com <none@none>
2011-03-19 16:36:41 -0700

    [iPhoneXMPP]  Added a settings flip view, users no longer need to hardcode JID and password before building.
    Add XEP-0054 and XEP-0153 to the demo app.  The app will download vCards and display vCard Avatars for each user in the roster.
    
    --HG--
    branch : XEP-0054_refactor

--- Xcode/iPhoneXMPP/Classes/iPhoneXMPPAppDelegate.h
+++ Xcode/iPhoneXMPP/Classes/iPhoneXMPPAppDelegate.h
@@ -1,9 +1,12 @@
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
+@class SettingsViewController;
 @class XMPPStream;
 @class XMPPRoster;
 @class XMPPRosterCoreDataStorage;
+@class XMPPvCardAvatarModule;
+@class XMPPvCardTempModule;
 
 
 @interface iPhoneXMPPAppDelegate : NSObject <UIApplicationDelegate>
@@ -11,6 +14,8 @@
 	XMPPStream *xmppStream;
 	XMPPRoster *xmppRoster;
 	XMPPRosterCoreDataStorage *xmppRosterStorage;
+  XMPPvCardAvatarModule *_xmppvCardAvatarModule;
+  XMPPvCardTempModule *_xmppvCardTempModule;
 	
 	NSString *password;
 	
@@ -21,13 +26,28 @@
 	
 	UIWindow *window;
 	UINavigationController *navigationController;
+  UIBarButtonItem *_loginButton;
+  
+  SettingsViewController *_loginViewController;
 }
 
 @property (nonatomic, readonly) XMPPStream *xmppStream;
 @property (nonatomic, readonly) XMPPRoster *xmppRoster;
 @property (nonatomic, readonly) XMPPRosterCoreDataStorage *xmppRosterStorage;
+@property (nonatomic, readonly) XMPPvCardAvatarModule *xmppvCardAvatarModule;
+@property (nonatomic, readonly) XMPPvCardTempModule *xmppvCardTempModule;
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
 
+@property (nonatomic, retain) IBOutlet UIBarButtonItem *loginButton;
+@property (nonatomic, retain) IBOutlet SettingsViewController *settingsViewController;
+
+- (BOOL)connect;
+- (void)disconnect;
+
+- (void)goOnline;
+- (void)goOffline;
+
+- (void)setupStream;
 @end
