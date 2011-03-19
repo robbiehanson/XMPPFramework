9d58c0a02251b93d2e2011f7b30daa107627e6bf
eric.chamberlain@gmail.com <none@none>
2011-03-19 16:30:29 -0700

    [XEP-0054] Fix photoData, don't try and create a XMPPvCardAvatarCoreDataStore object when there is no photoData to save.
    --HG--
    branch : XEP-0054_refactor

--- Extensions/XEP-0054/XMPPvCardCoreDataStorage.h
+++ Extensions/XEP-0054/XMPPvCardCoreDataStorage.h
@@ -21,14 +21,50 @@
 }
 
 
+/*
+ *  user's JID, indexed for lookups
+ */
 @property (nonatomic, retain) NSString * jidStr;
+
+
+/*
+ *  user's photoHash used by XEP-0153
+ */
 @property (nonatomic, retain, readonly) NSString * photoHash;
+
+/*
+ *  the last time the record was modified, also used to determine if we need to fetch again
+ */
 @property (nonatomic, retain) NSDate * lastUpdated;
+
+
+/*
+ *  flag indicating whether a get request is already pending, used in conjunction with lastUpdated
+ */
 @property (nonatomic, retain) NSNumber * waitingForFetch;
+
+
+/*
+ *  Relationship to the vCardTemp record.  We use a relationship, so the vCardTemp stays faulted until we really need it.
+ */
 @property (nonatomic, retain) XMPPvCardTempCoreDataStorage * vCardTempRel;
+
+
+/*
+ *  Relationship to the vCardAvatar record.  We use a relationship, so the vCardAvatar stays faulted until we really need it.
+ */
 @property (nonatomic, retain) XMPPvCardAvatarCoreDataStorage * vCardAvatarRel;
 
+
+/*
+ *  Accessor to retrieve photoData, so we can hide the underlying relationship implementation.
+ */
 @property (nonatomic, retain) NSData *photoData;
+
+
+/*
+ *  Accessor to retrieve vCardTemp, so we can hide the underlying relationship implementation.
+ */
 @property (nonatomic, retain) XMPPvCardTemp *vCardTemp;
 
 
