15a94aff0819f0f1a85c7dba52a5b239f615ff55
Robbie Hanson <robbiehanson@deusty.com>
2011-03-28 18:10:59 -0400

    Initial port of XEP-0054 (vCard) to v3 architecture.
    --HG--
    branch : v3

---
+++ Extensions/XEP-0054/XMPPvCardTempCoreDataStorageObject.h
@@ -0,0 +1,23 @@
+//
+//  XMPPvCardTempCoreDataStorageObject.h
+//  XEP-0054 vCard-temp
+//
+//  Oringally created by Eric Chamberlain on 3/18/11.
+//
+//  This class is so that we don't load the vCardTemp each time we need to touch the XMPPvCardCoreDataStorageObject.
+//  The vCardTemp abstraction also makes it easier to eventually add support for vCard4 over XMPP (XEP-0292).
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "XMPPvcardTemp.h"
+
+@class XMPPvCardCoreDataStorageObject;
+
+
+@interface XMPPvCardTempCoreDataStorageObject : NSManagedObject
+
+@property (nonatomic, retain) XMPPvCardTemp * vCardTemp;
+@property (nonatomic, retain) XMPPvCardCoreDataStorageObject * vCard;
+
+@end
