6f4c87d012bf39ccbd75ea013ed44e66e345072f
Eric Chamberlain <eric@rf.com>
2011-03-31 19:12:42 -0700

    [XEP-0054] Fix semantic issue errors.

--- Extensions/XEP-0054/XMPPvCardTempEmail.h
+++ Extensions/XEP-0054/XMPPvCardTempEmail.h
@@ -16,11 +16,11 @@
 @interface XMPPvCardTempEmail : XMPPvCardTempBase
 
 
-@property (nonatomic, assign, setter=setHome) BOOL isHome;
-@property (nonatomic, assign, setter=setWork) BOOL isWork;
-@property (nonatomic, assign, setter=setInternet) BOOL isInternet;
-@property (nonatomic, assign, setter=setX400) BOOL isX400;
-@property (nonatomic, assign, setter=setPreferred) BOOL isPreferred;
+@property (nonatomic, assign, setter=setHome:) BOOL isHome;
+@property (nonatomic, assign, setter=setWork:) BOOL isWork;
+@property (nonatomic, assign, setter=setInternet:) BOOL isInternet;
+@property (nonatomic, assign, setter=setX400:) BOOL isX400;
+@property (nonatomic, assign, setter=setPreferred:) BOOL isPreferred;
 @property (nonatomic, assign) NSString *userid;
 
 
