923a02c1672f51ba57a600226c530f8ca40b1a1d
Eric Chamberlain <eric@rf.com>
2011-03-31 19:12:42 -0700

    [XEP-0054] Fix semantic issue errors.
    --HG--
    branch : v3

--- Extensions/XEP-0054/XMPPvCardTempAdrTypes.h
+++ Extensions/XEP-0054/XMPPvCardTempAdrTypes.h
@@ -16,13 +16,13 @@
 @interface XMPPvCardTempAdrTypes : XMPPvCardTempBase
 
 
-@property (nonatomic, assign, setter=setHome) BOOL isHome;
-@property (nonatomic, assign, setter=setWork) BOOL isWork;
-@property (nonatomic, assign, setter=setParcel) BOOL isParcel;
-@property (nonatomic, assign, setter=setPostal) BOOL isPostal;
-@property (nonatomic, assign, setter=setDomestic) BOOL isDomestic;
-@property (nonatomic, assign, setter=setInternational) BOOL isInternational;
-@property (nonatomic, assign, setter=setPreferred) BOOL isPreferred;
+@property (nonatomic, assign, setter=setHome:) BOOL isHome;
+@property (nonatomic, assign, setter=setWork:) BOOL isWork;
+@property (nonatomic, assign, setter=setParcel:) BOOL isParcel;
+@property (nonatomic, assign, setter=setPostal:) BOOL isPostal;
+@property (nonatomic, assign, setter=setDomestic:) BOOL isDomestic;
+@property (nonatomic, assign, setter=setInternational:) BOOL isInternational;
+@property (nonatomic, assign, setter=setPreferred:) BOOL isPreferred;
 
 
 @end
