b51b261ce977750b85f0b44684730a297caa4cff
Robbie Hanson <robbiehanson@deusty.com>
2011-04-13 16:25:58 -0400

    Removing SSCrypto - CommonCrypto is always available, and is faster.

---
+++ Xcode/Testing/ParserTestMac/ParserTestMac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ParserTestMac
+//
+//  Created by Robbie Hanson on 4/13/11.
+//  Copyright 2011 Voalte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
