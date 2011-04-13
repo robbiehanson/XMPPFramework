2e67c5c4352c49234dd02ce8aad82105e7b791db
Robbie Hanson <robbiehanson@deusty.com>
2011-04-13 10:37:38 -0400

    [XMPPPing] Adding a respondsToQueries property to make it possible to create multiple instances of the module.
    --HG--
    branch : v3

--- Extensions/XEP-0199/XMPPPing.h
+++ Extensions/XEP-0199/XMPPPing.h
@@ -9,8 +9,18 @@
 
 @interface XMPPPing : XMPPModule
 {
+	BOOL respondsToQueries;
 	NSMutableDictionary *pingIDs;
 }
+
+/**
+ * Whether or not the module should respond to incoming ping queries.
+ * It you create multiple instances of this module, only one instance should respond to queries.
+ * 
+ * It is recommended you set this (if needed) before you activate the module.
+ * The default value is YES.
+**/
+@property (readwrite) BOOL respondsToQueries;
 
 /**
  * Send pings to the server or a specific JID.
