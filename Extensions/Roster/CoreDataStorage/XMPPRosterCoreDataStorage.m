d95ecc2791bc4ebdb8598a1a26977a73a671bae1
Robbie Hanson <robbiehanson@deusty.com>
2011-12-02 13:26:16 -0800

    Turning down logging

--- Extensions/Roster/CoreDataStorage/XMPPRosterCoreDataStorage.m
+++ Extensions/Roster/CoreDataStorage/XMPPRosterCoreDataStorage.m
@@ -14,7 +14,7 @@
 
 // Log levels: off, error, warn, info, verbose
 #if DEBUG
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_INFO | XMPP_LOG_FLAG_TRACE;
+  static const int xmppLogLevel = XMPP_LOG_LEVEL_INFO; // | XMPP_LOG_FLAG_TRACE;
 #else
   static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 #endif
