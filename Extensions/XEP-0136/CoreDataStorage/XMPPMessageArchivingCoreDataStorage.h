b9739f3b343f410e91b7927c7c5024e586d87726
Robbie Hanson <robbiehanson@deusty.com>
2012-02-20 12:56:57 -0800

    Beginning work on XEP-0136 : Message Archiving

---
+++ Extensions/XEP-0136/CoreDataStorage/XMPPMessageArchivingCoreDataStorage.h
@@ -0,0 +1,51 @@
+#import <Foundation/Foundation.h>
+
+#import "XMPPCoreDataStorage.h"
+#import "XMPPMessageArchiving.h"
+#import "XMPPMessageArchivingCoreDataStorageObject.h"
+
+
+@interface XMPPMessageArchivingCoreDataStorage : XMPPCoreDataStorage <XMPPMessageArchivingStorage>
+{
+	/* Inherited protected variables from XMPPCoreDataStorage
+	
+	NSString *databaseFileName;
+	NSUInteger saveThreshold;
+	
+	dispatch_queue_t storageQueue;
+	 
+	*/
+}
+
+/**
+ * Convenience method to get an instance with the default database name.
+ * 
+ * IMPORTANT:
+ * You are NOT required to use the sharedInstance.
+ * 
+ * If your application uses multiple xmppStreams, and you use a sharedInstance of this class,
+ * then all of your streams share the same database store. You might get better performance if you create
+ * multiple instances of this class instead (using different database filenames), as this way you can have
+ * concurrent writes to multiple databases.
+**/
++ (XMPPMessageArchivingCoreDataStorage *)sharedInstance;
+
+
+/* Inherited from XMPPCoreDataStorage
+ * Please see the XMPPCoreDataStorage header file for extensive documentation.
+ 
+- (id)initWithDatabaseFilename:(NSString *)databaseFileName;
+- (id)initWithInMemoryStore;
+
+@property (readonly) NSString *databaseFileName;
+ 
+@property (readwrite) NSUInteger saveThreshold;
+
+@property (readonly) NSManagedObjectModel *managedObjectModel;
+@property (readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (readonly) NSManagedObjectContext *mainThreadManagedObjectContext;
+ 
+*/
+
+@end
