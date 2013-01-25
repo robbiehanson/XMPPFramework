6c6478ec7faf14d7980180193e620000f5e54159
Paul Melnikow <email@paulmelnikow.com>
2013-01-25 15:33:34 -0500

    Add support to XMPPReconnect for -oldSchoolSecureConnect

--- Extensions/Reconnect/XMPPReconnect.m
+++ Extensions/Reconnect/XMPPReconnect.m
@@ -625,7 +625,14 @@ static void ReachabilityChanged(SCNetworkReachabilityRef target, SCNetworkReacha
 						[self setMultipleReachabilityChanges:NO];
 						previousReachabilityFlags = reachabilityFlags;
 						
-						[xmppStream connect:nil];
+                        if (self.usesOldSchoolSecureConnect)
+                        {
+                            [xmppStream oldSchoolSecureConnect:nil];
+                        }
+                        else
+                        {
+                            [xmppStream connect:nil];
+                        }
 					}
 					else if ([self multipleReachabilityChanges])
 					{
