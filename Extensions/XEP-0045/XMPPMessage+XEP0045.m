2fbfcb5c50ef4a9384da3be76526d94c71b1ebac
Matt Johnson <Matt.Johnson@cliqueintelligence.com>
2013-08-01 10:53:10 -0700

    XEP-0045: added methods to XMPPMessage category to handle room subjects, have XMPPRoom check incoming messages for room subjects and use them to set the roomSubject property

--- Extensions/XEP-0045/XMPPMessage+XEP0045.m
+++ Extensions/XEP-0045/XMPPMessage+XEP0045.m
@@ -21,4 +21,26 @@
 	return NO;
 }
 
+- (BOOL)isGroupChatMessageWithSubject
+{
+    if ([self isGroupChatMessage]) {
+        NSString *subject = [[self elementForName:@"subject"] stringValue];
+
+		return ((subject != nil) && ([subject length] > 0));
+    }
+
+    return NO;
+}
+
+- (NSString *)subject
+{
+	return [[self elementForName:@"subject"] stringValue];
+}
+
+- (void)addSubject:(NSString *)subject
+{
+    NSXMLElement *subjectElement = [NSXMLElement elementWithName:@"subject" stringValue:subject];
+    [self addChild:subjectElement];
+}
+
 @end
