b9cedbf57c00dfb56337b613c3acfd4605cf506f
Joao Nunes <joao@Joaos-MacBook-Pro.local>
2014-03-19 16:33:46 +0200

    Removed compiler warnings from unused xmpplog variables

--- Core/XMPPModule.m
+++ Core/XMPPModule.m
@@ -6,12 +6,6 @@
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
 
-// Log levels: off, error, warn, info, verbose
-#if DEBUG
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
-#else
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
-#endif
 
 @implementation XMPPModule
 
--- Core/XMPPParser.m
+++ Core/XMPPParser.m
@@ -11,12 +11,6 @@
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
 
-// Log levels: off, error, warn, info, verbose
-#if DEBUG
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_VERBOSE;
-#else
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
-#endif
 
 #define CHECK_FOR_NULL(value)                       \
     do {                                            \
