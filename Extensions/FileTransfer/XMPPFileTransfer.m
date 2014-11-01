628b9423793bf751a8b1bfae2c2262ab0a96a8b5
Jonathon Staff <jon@nplexity.com>
2014-11-01 19:33:59 +0800

    Added FileTransfer extension

---
+++ Extensions/FileTransfer/XMPPFileTransfer.m
@@ -0,0 +1,11 @@
+//
+// Created by Jonathon Staff on 10/21/14.
+// Copyright (c) 2014 nplexity, LLC. All rights reserved.
+//
+
+#import "XMPPFileTransfer.h"
+
+
+@implementation XMPPFileTransfer
+
+@end
