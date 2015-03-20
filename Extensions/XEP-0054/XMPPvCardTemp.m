6b1348e1cd58ed3355a358e4529869ba96afe4c2
Spencer MacDonald <spencer.macdonald@gmail.com>
2015-03-20 15:09:33 +0000

    Fixes #511

--- Extensions/XEP-0054/XMPPvCardTemp.m
+++ Extensions/XEP-0054/XMPPvCardTemp.m
@@ -21,8 +21,6 @@
 
 #if DEBUG
   static const int xmppLogLevel = XMPP_LOG_LEVEL_ERROR;
-#else
-  static const int xmppLogLevel = XMPP_LOG_LEVEL_ERROR;
 #endif
 
 NSString *const kXMPPNSvCardTemp = @"vcard-temp";
