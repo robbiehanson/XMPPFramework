ab53f94d253190c72ba2620a459eacd4cd95a5bb
Paul Melnikow <email@paulmelnikow.com>
2015-04-18 13:02:11 -0400

    Add a new test project, migrate EncodeDecodeTest

---
+++ Xcode/Testing2/XMPPFrameworkTests/XMPPFrameworkTests/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPPFrameworkTests
+//
+//  Created by Paul Melnikow on 4/18/15.
+//  Copyright (c) 2015 Paul Melnikow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
