d3495d27c37950336f11f61affb3c438915cbf76
Spencer MacDonald <spencer.macdonald@gmail.com>
2015-06-03 22:14:35 +0100

    XMPPSASLAuthentication Fixes
    XMPPSASLAuthentication initWithStream:username:password: is now Optional

--- Authentication/XMPPSASLAuthentication.h
+++ Authentication/XMPPSASLAuthentication.h
@@ -58,12 +58,6 @@ typedef NS_ENUM(NSInteger, XMPPHandleAuthResponse) {
 **/
 - (id)initWithStream:(XMPPStream *)stream password:(NSString *)password;
 
-/**
- * Use this init method if the username used for authentication does not match the user part of the JID.
- * If username is nil, the user part of the JID will be used.
- * The standard init method uses this init method, passing nil for the username.
-**/
-- (id)initWithStream:(XMPPStream *)stream username:(NSString *)username password:(NSString *)password;
 
 /**
  * Attempts to start the authentication process.
@@ -91,6 +85,13 @@ typedef NS_ENUM(NSInteger, XMPPHandleAuthResponse) {
 - (XMPPHandleAuthResponse)handleAuth:(NSXMLElement *)auth;
 
 @optional
+
+/**
+ * Use this init method if the username used for authentication does not match the user part of the JID.
+ * If username is nil, the user part of the JID will be used.
+ * The standard init method uses this init method, passing nil for the username.
+ **/
+- (id)initWithStream:(XMPPStream *)stream username:(NSString *)username password:(NSString *)password;
 
 /**
  * Optionally implement this method to override the default behavior.
