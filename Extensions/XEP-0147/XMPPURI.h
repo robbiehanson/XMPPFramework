4f495449b3edc03d4df34068ef76cebc315f579c
Chris Ballinger <chrisballinger@gmail.com>
2015-06-12 17:08:25 -0700

    XEP-0147: Change license header

--- Extensions/XEP-0147/XMPPURI.h
+++ Extensions/XEP-0147/XMPPURI.h
@@ -1,6 +1,6 @@
 //
 //  XMPPURI.h
-//  ChatSecure
+//  XMPPFramework
 //
 //  Created by Christopher Ballinger on 5/15/15.
 //  Copyright (c) 2015 Chris Ballinger. All rights reserved.
--- Extensions/XEP-0147/XMPPURI.m
+++ Extensions/XEP-0147/XMPPURI.m
@@ -1,6 +1,6 @@
 //
 //  XMPPURI.m
-//  ChatSecure
+//  XMPPFramework
 //
 //  Created by Christopher Ballinger on 5/15/15.
 //  Copyright (c) 2015 Chris Ballinger. All rights reserved.
