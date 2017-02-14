cc7c02c505507535eb9ca34532ed03eb7a61205e
Alexander Dodatko <alexander.dodatko@dataart.com>
2017-02-14 19:06:46 +0200

    [framework] added project stub

---
+++ AAA__LibProjects/XMPPFramework-iOS/XMPPFramework-iOS/XMPPFramework-iOS.h
@@ -0,0 +1,19 @@
+//
+//  XMPPFramework-iOS.h
+//  XMPPFramework-iOS
+//
+//  Created by Alexander Dodatko on 2/14/17.
+//  Copyright © 2017 robbiehanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMPPFramework-iOS.
+FOUNDATION_EXPORT double XMPPFramework_iOSVersionNumber;
+
+//! Project version string for XMPPFramework-iOS.
+FOUNDATION_EXPORT const unsigned char XMPPFramework_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPPFramework_iOS/PublicHeader.h>
+
+
