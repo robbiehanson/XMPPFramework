fe1e270547505f08f2eaa49e322a0bf6b3d616c0
Piotr Wegrzynek <pwe@trifork.com>
2017-09-28 11:02:34 +0200

    Basic one to one chat module implementation

--- Core/XMPPFramework.h
+++ Core/XMPPFramework.h
@@ -159,6 +159,7 @@
 #import "XMPPRoomLightCoreDataStorage.h"
 #import "XMPPRoomLightCoreDataStorageProtected.h"
 #import "XMPPRoomLightMessageCoreDataStorageObject.h"
+#import "XMPPOneToOneChat.h"
 
 
 FOUNDATION_EXPORT double XMPPFrameworkVersionNumber;
