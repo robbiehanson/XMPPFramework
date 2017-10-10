a1a1001637369bd630d1e4c468d060ed4749a531
Chris Ballinger <chrisballinger@gmail.com>
2017-10-09 17:24:16 -0700

    Auditing XEP-0352, XEP-0335

--- Extensions/XEP-0352/NSXMLElement+XEP_0352.h
+++ Extensions/XEP-0352/NSXMLElement+XEP_0352.h
@@ -1,9 +1,11 @@
 
 #import "NSXMLElement+XMPP.h"
 
+NS_ASSUME_NONNULL_BEGIN
 @interface NSXMLElement (XEP0352)
 
 + (instancetype)indicateInactiveElement;
 + (instancetype)indicateActiveElement;
 
 @end
+NS_ASSUME_NONNULL_END
