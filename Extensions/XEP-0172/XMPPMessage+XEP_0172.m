6064b975c739d18bd24ec4d805efcf6c61d64ab7
John Flanagan <john.flanagan@spok.com>
2018-03-08 13:44:59 -0600

    Add ability to set "nick" for XEP_0172

--- Extensions/XEP-0172/XMPPMessage+XEP_0172.m
+++ Extensions/XEP-0172/XMPPMessage+XEP_0172.m
@@ -10,4 +10,11 @@
 	return [[self elementForName:@"nick" xmlns:XMLNS_NICK] stringValue];
 }
 
+- (void)addNick:(NSString *)nick
+{
+    NSXMLElement *nickElement = [NSXMLElement elementWithName:@"nick" xmlns:XMLNS_NICK];
+    [nickElement addChild:[NSXMLNode textWithStringValue:nick]];
+    [self addChild:nickElement];
+}
+
 @end
